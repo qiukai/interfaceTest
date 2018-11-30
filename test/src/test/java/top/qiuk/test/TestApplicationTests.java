32da02a000c755d8e9405ef7fbaf01f5371f9998
邱凯 <kai.qiu@lanmaoly.com>
2018-11-30 18:45:49 +0800

    添加日志打印 logback

--- test/src/test/java/top/qiuk/test/TestApplicationTests.java
+++ test/src/test/java/top/qiuk/test/TestApplicationTests.java
@@ -2,15 +2,23 @@ package top.qiuk.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestApplicationTests {
 
+    private static Logger logger = LoggerFactory.getLogger(TestApplicationTests.class);
+
+
     @Test
     public void contextLoads() {
+
+        logger.info("123");
+
     }
 
 }
